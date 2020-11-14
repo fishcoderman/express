62f548a5e176ba927182c27856c265a7b6ba6d39
ChinaMasters <1263641416@qq.com>
2020-11-14 21:19:20 +0800

    ffffffff

---
+++ index.js
@@ -0,0 +1,5 @@
+const data  = {}
+
+// git config --global user.name "ChinaMasters"
+
+// git config --global user.email 1263641416@qq.com 
--- router/user.js
+++ router/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const route = express.Router()
 
-route.post('/infos', (req, res) => {
+route.post('/info', (req, res) => {
   const {appKey, appSecret} = req.body
   console.log('req', req.body)
   res.send({
